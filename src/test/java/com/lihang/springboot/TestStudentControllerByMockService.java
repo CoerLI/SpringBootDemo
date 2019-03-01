5a86f603aca512353cd4a347e951fb7a9f0e0524
lihang <lihang@huobi.com>
2019-03-01 19:59:04 +0800

    commit 3-1

--- src/test/java/com/lihang/springboot/TestStudentControllerByMockService.java
+++ src/test/java/com/lihang/springboot/TestStudentControllerByMockService.java
@@ -8,10 +8,8 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.web.context.WebApplicationContext;
 
 import java.util.ArrayList;
 
